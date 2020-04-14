484d88195ae241b5e9dd6bf0ee59d899a0e9561e
EMIRATES\S460242 <vivek.chauhan@emirates.com>
2020-04-14 23:58:56 +0400

    Rmeove the unused file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Hotel from "../components/hotel/index";
-require("./index.css");
 
 const rootEl = document.getElementById("app");
 
 ReactDOM.render(<Hotel />, rootEl);
-
-if (module.hot) {
-  module.hot.accept();
-}
